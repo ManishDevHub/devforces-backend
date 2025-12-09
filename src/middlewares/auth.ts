3434737335a95d742ce5474d1755a4e53dc01ebf
YourGitHubUsername <ramuk13476@gmail.com>
2025-12-09 20:04:29 +0530

    add auth middleware

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,40 @@
+
+import { Request , Response , NextFunction } from "express";
+import jwt from 'jsonwebtoken'
+import { any } from "zod";
+import { de } from "zod/v4/locales";
+
+
+const JWT_SECRET = process.env.JWT_SECRET || "sdklfhdfjdsfd"
+
+interface AuthRequest extends Request {
+    user?: any
+}
+
+ export const auth = (req: AuthRequest , res: Response , next: NextFunction) =>{
+
+    try{
+         const header = req.headers.authorization;
+
+    if(!header || !header.startsWith("Bearer ")){
+        return res.status(401).json({message: "Unauthorized: Invalid or expire token "})
+    }
+
+    const token = header.split(" ")[1]
+    const decode = jwt.verify(token , JWT_SECRET);
+
+    if(!decode){
+        return res.status(401).json({message: "Unauthorized or expire token "})
+    }
+        req.user = decode,
+     next();
+
+    }catch(error){
+        console.error("Auth error :" , error);
+        return res.json({message: "Unauthorized access"})
+
+    }
+
+   
+
+}
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { login, register } from "../controllers/user.controller"
 import { validate } from "../middlewares/validate";
 import { loginSchema, registerSchema } from "../validations/auth.schema";
+import { auth} from '../middlewares/auth'
 
 
 const router = Router();
 router.post('/register' , validate(registerSchema), register);
 router.post('/login' , validate(loginSchema), login)
+
 
 export default router;
